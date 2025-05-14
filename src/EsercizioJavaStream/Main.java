f659da510cab327e97a7e9a76790c9234bafea54
AngeloZorzi <astro.drg666@gmail.com>
2025-05-14 15:46:08 +0200

    Adds classes and products

---
+++ src/EsercizioJavaStream/Main.java
@@ -0,0 +1,16 @@
+package EsercizioJavaStream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Product p1 = new Product(1L,"Geronimo Stilton 1st ed","Books",150.25);
+        Product p2 = new Product(2L,"World at war","Books",75.0);
+        Product p3 = new Product(3L,"A DRG Story","Books",275.0);
+        Product p4 = new Product(4L,"MachineGun","Boys",100.0);
+        Product p5 = new Product(5L,"Fobal","Boys",15.0);
+        Product p6 = new Product(6L,"How to survive against squirrels/Doom-Pack","Boys",975.0);
+        Product p7 = new Product(7L,"Silencer","Baby",55.0);
+        Product p8 = new Product(8L,"How To Make your baby cry","Baby",9.0);
+        Product p9 = new Product(9L,"Baby-Monitor","Baby",250.0);
+    }
+}
---
+++ src/EsercizioJavaStream/Product.java
@@ -0,0 +1,22 @@
+package EsercizioJavaStream;
+
+public class Product {
+    Long id;
+    String name;
+    String category;
+    Double price;
+
+    public Product(Long id, String name, String category, Double price) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public String getCategory() { return category; }
+    public Double getPrice() { return price; }
+    public void setPrice(Double price) { this.price = price; }
+    public String toString() {
+        return name + " (" + category + ", €" + price + ")";
+    }
+}
