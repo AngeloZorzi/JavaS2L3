9e15691d2432c13405de1bded4f993baabecf82c
AngeloZorzi <astro.drg666@gmail.com>
2025-05-14 16:06:07 +0200

    #1

--- src/EsercizioJavaStream/Main.java
+++ src/EsercizioJavaStream/Main.java
@@ -1,5 +1,10 @@
 package EsercizioJavaStream;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -12,5 +17,20 @@ public class Main {
         Product p7 = new Product(7L,"Silencer","Baby",55.0);
         Product p8 = new Product(8L,"How To Make your baby cry","Baby",9.0);
         Product p9 = new Product(9L,"Baby-Monitor","Baby",250.0);
+
+        List<Product> products = new ArrayList<>();
+        products.add(p1);
+        products.add(p2);
+        products.add(p3);
+        products.add(p4);
+        products.add(p5);
+        products.add(p6);
+        products.add(p7);
+        products.add(p8);
+        products.add(p9);
+
+        List<Product> over100 = products.stream().filter(product -> product.getCategory().equals("Books")&&product.getPrice()>100).collect(Collectors.toList());
+        System.out.println("Books that costs as my grandpa would say 'Più di una gamba'");
+        over100.forEach(System.out::println);
     }
 }
